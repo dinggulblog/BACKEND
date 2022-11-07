b3f3537de65e046544cc84362f22726cef952d23
Sudal <56054637+Sudal1@users.noreply.github.com>
2022-11-07 16:29:32 +0900

    Add method to insert default menu documents

--- config/mongo.js
+++ config/mongo.js
@@ -16,20 +16,33 @@ export const connectMongoDB = async (url) => {
 }
 
 export const createDefaultDocuments = async () => {
+  const roles = ['USER', 'MODERATOR', 'ADMIN'];
+  const menus = { 
+    'sol': [
+      { sub: 'dev', categories: ['기타', 'Javascript', 'Vue.js', 'CSS'] },
+      { sub: 'daily', categories: ['기타', '일상', '햄찌', '게임'] },
+      { sub: 'album', categories: ['기타', '햄찌', '맛집', '여행'] }
+    ],
+    'ming': [
+      { sub: 'dev', categories: ['기타', 'Javascript', 'Node.js', 'MongoDB'] },
+      { sub: 'daily', categories: ['기타', '일상', '햄찌', '게임'] },
+      { sub: 'album', categories: ['기타', '햄찌', '게임', '여행'] }
+    ],
+    'guest': [
+      { sub: 'guest book' }
+    ]
+  };
+
   try {
     if (!await RoleModel.estimatedDocumentCount()) {
-      await Promise.all([
-        new RoleModel({ name: 'USER' }).save(),
-        new RoleModel({ name: 'MODERATOR' }).save(),
-        new RoleModel({ name: 'ADMIN' }).save()
-      ])
+      await Promise.all(
+        roles.map(async name => await new RoleModel({ name }).save())
+      );
     }
     if (!await MenuModel.estimatedDocumentCount()) {
-      await Promise.all([
-        new MenuModel({ main: 'sol' }).save(),
-        new MenuModel({ main: 'ming' }).save(),
-        new MenuModel({ main: 'guest' }).save()
-      ])
+      await Promise.all(
+        Object.keys(menus).map(main => menus[main].map(async ({ sub, categories }) => await new MenuModel({ main, sub, categories }).save())).flat()
+      );
     }
   } catch (error) {
     console.error('MongoDB initiation ERROR: ' + error);
