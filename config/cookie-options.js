1918f9ea7b1a6ebf1544a93b8f67f38cb309bee6
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-01-07 16:45:10 +0900

    Change cookie options

--- config/cookie-options.js
+++ config/cookie-options.js
@@ -2,6 +2,7 @@ export const cookieOption = (time) => {
   return {
     httpOnly: true,
     maxAge: time,
+    domain: 'dinggul.me',
     signed: process.env.NODE_ENV !== 'develop',
     secure: process.env.NODE_ENV !== 'develop',
     proxy: process.env.NODE_ENV !== 'develop'
