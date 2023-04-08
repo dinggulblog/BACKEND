233c334b9f2473f5f7b3313bc115556b3c72418f
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-08 22:29:25 +0900

    Set csp options false

--- index.mjs
+++ index.mjs
@@ -23,7 +23,6 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.enable('trust proxy');
   app.use(morgan('combined'));
-  app.use(helmet.contentSecurityPolicy({ directives: cspOptions }))
   app.use(helmet.crossOriginEmbedderPolicy({ policy: 'credentialless' }));
   app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
   app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, includeSubDomains: true, preload: true }));
