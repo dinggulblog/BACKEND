ebacd8605c2973767cc73539c47ebf8ef26291a2
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-08 22:11:33 +0900

    Fix typo

--- config/csp-options.js
+++ config/csp-options.js
@@ -1,5 +1,5 @@
 export const cspOptions = {
   'script-src': ["'self'", "*.googleapis.com", "'unsafe-inline'", "'unsafe-eval'"],
   'img-src': ["'self'", "data:", "*.dinggul.me"],
-  'img-src': ["'self'", "https:", "*.dinggul.me", "'unsafe-inline'"],
+  'style-src': ["'self'", "https:", "*.dinggul.me", "'unsafe-inline'"],
 };
