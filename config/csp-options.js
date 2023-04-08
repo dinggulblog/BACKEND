03526398419b3bebd1d14cfe59ba90738e8fa065
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-08 22:53:02 +0900

    update csp options'

--- config/csp-options.js
+++ config/csp-options.js
@@ -1,5 +1,5 @@
 export const cspOptions = {
-  'base-uri' : ["/", "http:"],
-  'script-src': ["'self'", "*.googleapis.com", "'unsafe-inline'", "'unsafe-eval'"],
+  'script-src': ["'self'", "*.googleapis.com", "*.cloudflare.com", "'unsafe-inline'", "'unsafe-eval'"],
   'img-src': ["'self'", "data:", "*.dinggul.me"],
+  'font-src': ["'self'", "data:", "https:", "*.alicdn.com"]
 };
