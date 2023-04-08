8b5d56c0d7b04c236900b21a096a713061eee2b1
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-09 05:16:10 +0900

    Test

--- index.mjs
+++ index.mjs
@@ -23,19 +23,7 @@ const app = express();
 if (process.env.NODE_ENV === 'production') {
   app.enable('trust proxy');
   app.use(morgan('combined'));
-  app.use(helmet.contentSecurityPolicy({ directives: cspOptions }))
-  app.use(helmet.crossOriginEmbedderPolicy({ policy: 'credentialless' }));
-  app.use(helmet.crossOriginResourcePolicy({ policy: 'same-origin' }));
-  app.use(helmet.hsts({ maxAge: 90 * 24 * 60 * 60, includeSubDomains: true, preload: true }));
-  app.use(helmet.dnsPrefetchControl());
-  app.use(helmet.frameguard());
-  app.use(helmet.hidePoweredBy());
-  app.use(helmet.ieNoOpen());
-  app.use(helmet.noSniff());
-  app.use(helmet.originAgentCluster());
-  app.use(helmet.permittedCrossDomainPolicies());
-  app.use(helmet.referrerPolicy());
-  app.use(helmet.xssFilter());
+  app.use(helmet({ contentSecurityPolicy: false }));
   app.use(hpp({ whitelist: ['menus'] }));
 } else {
   app.use(morgan('dev'));
