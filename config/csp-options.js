705c77aa2dc56aea74115832ac86f7a0f58d37c6
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-08 22:06:43 +0900

    Update csp style src option

--- config/csp-options.js
+++ config/csp-options.js
@@ -1,4 +1,5 @@
 export const cspOptions = {
   'script-src': ["'self'", "*.googleapis.com", "'unsafe-inline'", "'unsafe-eval'"],
-  'img-src': ["'self'", "data:", "*.dinggul.me"]
+  'img-src': ["'self'", "data:", "*.dinggul.me"],
+  'style-src': null,
 };
