8d7bef1c5bc4fe66786878126f4957f390772465
Sudal <56054637+Sudal1@users.noreply.github.com>
2023-04-09 04:38:38 +0900

    CSP test

--- config/csp-options.js
+++ config/csp-options.js
@@ -1,5 +1,5 @@
 export const cspOptions = {
   'script-src': ["'self'", "*.googleapis.com", "*.cloudflare.com", "*.alicdn.com", "'unsafe-inline'", "'unsafe-eval'"],
-  'img-src': ["'self'", "data:", "*.dinggul.me"],
+  'img-src': ["'self'", "data:", "*.dinggul.me", "https://dinggul-bucket.s3.ap-northeast-2.amazonaws.com"],
   'font-src': ["'self'", "data:", "https:", "*.alicdn.com"]
 };
